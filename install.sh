1e881ab11025912a8717f166d85e6dca79dd8682
Eelko Koppen <eelko@MBP-van-Eelko.localdomain>
2024-05-06 00:08:06 +0200

    fonts

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@
 echo "installing ssh config..."
 cp ./ssh/config ~/.ssh/config
 
+echo "installing fonts"
+unzip -o ./fonts/\*.zip -d ~/Library/Fonts
+
 #TOOLS
 echo "installing homebrew..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "installing zsh en voorkeuren..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
